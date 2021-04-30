7c4fe4533d5c12e154924c3980e0d680ac1b91e1
mychi darko <mickdd22@gmail.com>
2021-04-30 22:31:23 +0000

    :sparkles: added base controller

---
+++ Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controllers;
+
+/**
+ * This is the base controller for your Leaf API Project.
+ * You can initialize packages or define methods here to use
+ * them across all your other controllers which extend this one.
+ */
+class Controller extends \Leaf\Controller
+{
+    //
+}
