97574975c517eb3e0db4ea86090d8e70789f80a5
Mychi <mickdd22@gmail.com>
2021-10-10 20:28:09 +0000

    :ambulance: fixed view path

--- routes/_app.php
+++ routes/_app.php
@@ -1,7 +1,7 @@
 <?php
 
 app()->get("/", function () {
-    response()->page(views_path("index.html", false));
+    response()->page(viewsPath("index.html", false));
 });
 
 app()->get("/home", "TestsController@index");
