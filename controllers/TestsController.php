fb5483836556de32f331eddaa64d99375964f60d
Michael Darko <mickdd22@gmail.com>
2022-02-27 14:39:07 +0000

    :recycle: minor refactors

--- controllers/TestsController.php
+++ controllers/TestsController.php
@@ -10,37 +10,37 @@ class TestsController extends Controller
     {
         $data = [
             [
-                "icon" => "book-outline",
-                "title" => "Skeleton Documentation",
-                "description" => "Skeleton comes with a clean and developer-friendly documentation for both beginners and seasoned users.",
-                "link" => "https://leafphp.dev/skeleton/",
-                "linkText" => "Skeleton Docs"
+                'icon' => 'book-outline',
+                'title' => 'Skeleton Documentation',
+                'description' => 'Skeleton comes with a clean and developer-friendly documentation for both beginners and seasoned users.',
+                'link' => 'https://leafphp.dev/skeleton/',
+                'linkText' => 'Skeleton Docs'
             ],
             [
-                "icon" => "laptop-outline",
-                "title" => "Leaf Documentation",
-                "description" => "Since Skeleton is basically a boilerplate built with Leaf, we recommend checking out the leaf docs first.",
-                "link" => "https://leafphp.dev/",
-                "linkText" => "Leaf Docs"
+                'icon' => 'laptop-outline',
+                'title' => 'Leaf Documentation',
+                'description' => 'Since Skeleton is basically a boilerplate built with Leaf, we recommend checking out the leaf docs first.',
+                'link' => 'https://leafphp.dev/',
+                'linkText' => 'Leaf Docs'
             ],
             [
-                "icon" => "logo-twitter",
-                "title" => "Twitter",
-                "description" => "Follow Leaf PHP on Twitter to get latest news about releases, new modules, tutorials and amazing tips.",
-                "link" => "https://twitter.com/leafphp",
-                "linkText" => "@leafphp"
+                'icon' => 'logo-twitter',
+                'title' => 'Twitter',
+                'description' => 'Follow Leaf PHP on Twitter to get latest news about releases, new modules, tutorials and amazing tips.',
+                'link' => 'https://twitter.com/leafphp',
+                'linkText' => '@leafphp'
             ],
             [
-                "icon" => "logo-youtube",
-                "title" => "YouTube",
-                "description" => "We have a youtube channel where we upload videos on leaf, our modules, frameworks and other projects.",
-                "link" => "https://www.youtube.com/channel/UCllE-GsYy10RkxBUK0HIffw",
-                "linkText" => "Leaf YouTube Channel"
+                'icon' => 'logo-youtube',
+                'title' => 'YouTube',
+                'description' => 'We have a youtube channel where we upload videos on leaf, our modules, frameworks and other projects.',
+                'link' => 'https://www.youtube.com/channel/UCllE-GsYy10RkxBUK0HIffw',
+                'linkText' => 'Leaf YouTube Channel'
             ],
         ];
 
-        echo view("test", [
-            "data" => $data,
+        echo view('test', [
+            'data' => $data,
         ]);
     }
 
